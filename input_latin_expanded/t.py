7a4c324eb668075b3554aa647310b375de00bb48
Anurag Shah <33995099+Anurag-Shah@users.noreply.github.com>
2021-04-29 02:34:07 +0530

    Delete t.py

--- input_latin_expanded/t.py
+++
@@ -1,8 +0,0 @@
-import shutil, random, os
-dirpath = 'Cursive'
-destDirectory = 'tc'
-
-filenames = random.sample(os.listdir(dirpath), 600)
-for fname in filenames:
-    srcpath = os.path.join(dirpath, fname)
-    shutil.copyfile(srcpath, destDirectory + "/" + fname)
